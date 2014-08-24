ee6a4b77655ff99ee860f28033e03a0aff0bff08
Tobias Schlitt <toby@php.net>
2014-08-24 11:47:30 +0200

    Implemented: Much better debugging output.

--- src/main/Qafoo/SerPretty/Parser.php
+++ src/main/Qafoo/SerPretty/Parser.php
@@ -42,7 +42,7 @@ class Parser
         $this->currentIndex = 0;
         $this->maxIndex = strlen($serialized);
 
-        return $this->doParse($serialized);
+        return $this->doParse();
     }
 
     /**
@@ -50,8 +50,11 @@ class Parser
      */
     private function doParse()
     {
+
         while ($this->currentIndex < $this->maxIndex) {
             $dataType = $this->serialized[$this->currentIndex];
+            $this->debug('Detect: Data type "' . $dataType . '"');
+
             switch ($dataType) {
                 case 'i':
                     return $this->parseInt();
@@ -83,13 +86,15 @@ class Parser
      */
     private function parseString()
     {
-        // Skip ":"
+        $this->debug('String: Skip ":"');
         $this->advance(2);
 
         $string = $this->parseRawString();
 
-        // Skip "\";"
+        $this->debug('String: Skip ";"');
         $this->advance(2);
+
+        $this->debug('String: Parsed "' . $string . '"');
 
         return new Node\StringNode($string);
     }
@@ -99,16 +104,20 @@ class Parser
      */
     private function parseRawString()
     {
+        $this->debug('Raw String: Parse byte length');
         $stringLength = $this->parseRawInt();
+        $this->debug('Raw String: Byte length is ' . $stringLength);
 
-        // Skip ":\""
+        $this->debug('Raw String: Skip ":\""');
         $this->advance(3);
 
+        $this->debug('Raw String: Consuming ' . $stringLength . ' bytes');
         $string = $this->current();
         for ($i = 1; $i < $stringLength; $i++) {
             $this->advance();
             $string .= $this->current();
         }
+        $this->debug('Raw String: Parsed "' . $string . '"');
 
         return $string;
     }
@@ -120,6 +129,7 @@ class Parser
      */
     private function parseInt()
     {
+        $this->debug('Int: Skip "i:"');
         $this->advance(2);
 
         $integer = $this->parseRawInt();
@@ -136,6 +146,7 @@ class Parser
      */
     private function parseNull()
     {
+        $this->debug('Null: Skipping ";"');
         $this->advance(1);
 
         return new Node\NullNode();
@@ -148,6 +159,7 @@ class Parser
      */
     private function parseBoolean()
     {
+        $this->debug('Bool: Skipping "b:"');
         $this->advance(2);
 
         $value = (bool) $this->parseRawInt();
@@ -164,18 +176,27 @@ class Parser
      */
     private function parseArray()
     {
+        $this->debug('Array: Skipping "a:"');
         $this->advance(2);
 
         $arrayCount = $this->parseRawInt();
+        $this->debug('Array: Element count ' . $arrayCount);
 
+        $this->debug('Array: Skipping ":{"');
         $this->advance(3);
 
         $array = array();
         for ($i = 0; $i < $arrayCount; $i++) {
+            $this->debug('Array: Parsing element ' . $i);
+
             $key = $this->doParse();
+            $this->debug('Array: Parsed key "' . $key . '"');
+
             $this->advance();
 
             $value = $this->doParse();
+            $this->debug('Array: Parsed value "' . $value . '"');
+
             $this->advance();
 
             $array[] = new Node\ArrayElementNode($value, $key);
@@ -190,18 +211,24 @@ class Parser
      */
     private function parseObject()
     {
+        $this->debug('Object: Skipping ":""');
         $this->advance(2);
 
         $className = $this->parseRawString();
+        $this->debug('Object: Class name is "' . $className . '"');
 
+        $this->debug('Object: Skipping "":"');
         $this->advance(3);
 
         $numAttributes = $this->parseRawInt();
+        $this->debug('Object: Has ' . $numAttributes . ' attributes');
 
+        $this->debug('Object: Skipping ":{"');
         $this->advance(3);
 
         $attributes = array();
         for ($i = 0; $i < $numAttributes; $i++) {
+            $this->debug('Object: Parse attribute #' . $i);
             list($class, $name) = $this->parseAttributeName(
                 $this->doParse()
             );
@@ -223,14 +250,17 @@ class Parser
      */
     private function parseFloat()
     {
+        $this->debug('Float: Skipping ":"');
         $this->advance(2);
 
+        $this->debug('Float: Parsing');
         $float = '';
         do {
             $float .= $this->current();
             $this->advance();
         } while ($this->current() != ';');
 
+        $this->debug('Float: Parsed "' . $float . '"');
         return new Node\FloatNode((float) $float);
     }
 
@@ -241,6 +271,7 @@ class Parser
     private function parseAttributeName(Node\StringNode $stringNode)
     {
         $nameString = $stringNode->getContent();
+        $this->debug('Attribute Name: Parse from "' . $nameString . '"');
 
         if (substr($nameString, 0, 1) === "\0") {
             $nameString = substr($nameString, 1);
@@ -264,12 +295,15 @@ class Parser
      */
     private function parseRawInt()
     {
-        $integer = $this->current();
+        $this->debug('Raw Int: Parsing');
 
+        $integer = $this->current();
         while (ctype_digit($this->peek())) {
             $this->advance();
             $integer .= $this->current();
         }
+
+        $this->debug('Raw Int: Parsed ' . $integer);
 
         return (int) $integer;
     }
@@ -281,13 +315,8 @@ class Parser
      */
     private function advance($numChars = 1)
     {
-        $this->debug("Advance $numChars");
-
         $this->assertInBounds($numChars);
         $this->currentIndex += $numChars;
-
-        // Debugging
-        $this->current();
     }
 
     /**
@@ -297,7 +326,7 @@ class Parser
      */
     private function current()
     {
-        $this->debug('Current: ' . $this->serialized[$this->currentIndex]);
+        // $this->debug('Current: ' . $this->serialized[$this->currentIndex]);
 
         return $this->serialized[$this->currentIndex];
     }
@@ -340,7 +369,24 @@ class Parser
     private function debug($message)
     {
         if ($this->debug) {
-            printf("%s (%s)\n", $message, $this->getContext());
+            $stack = debug_backtrace();
+
+            $nestingLevel = 0;
+            for ($i = 0; $i < count($stack); $i++) {
+                if ($stack[$i]['function'] === 'parse') {
+                    break;
+                }
+                if (strpos($stack[$i]['function'], 'parse') === 0) {
+                    $nestingLevel++;
+                }
+            }
+
+            printf(
+                "%s%s (%s)\n",
+                str_repeat(' ', $nestingLevel),
+                $message,
+                $this->getContext()
+            );
         }
     }
 
@@ -358,7 +404,8 @@ class Parser
     private function getContext()
     {
         return sprintf(
-            'char #%d, context: …%s…',
+            'char "%s" (#%d), context: …%s…',
+            $this->serialized[$this->currentIndex],
             $this->currentIndex,
             substr($this->serialized, $this->currentIndex, 7)
         );
